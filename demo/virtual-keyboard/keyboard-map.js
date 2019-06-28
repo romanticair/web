0fee3da327ecb0df4e9074c4da808e47d9154d6e
romanticair <1024519570@qq.com>
2019-06-28 23:51:41 +0800

    virtual-keyboard

---
+++ demo/virtual-keyboard/keyboard-map.js
@@ -0,0 +1,29 @@
+const keyList = [
+  [
+    {char: '`', special: '~', code: 192}, {char: 1, special: '!', code: 49}, {char: 2, special: '@', code: 50}, {char: 3, special: '#', code: 51}, 
+    {char: 4, special: '$', code: 52}, {char: 5, special: '%', code: 53}, {char: 6, special: '^', code: 54}, {char: 7, special: '&', code: 55},
+    {char: 8, special: '*', code: 56}, {char: 9, special: '(', code: 57}, {char: 0, special: ')', code: 48}, {char: '-', special: '—', code: 173},
+    {char: '=', special: '+', code: 61}, {char: 'BackSpace', widthMultiple: 1.5, fontSize: '12px', code: 8}
+  ],
+  [
+    {char: 'Tab', widthMultiple: 1.5, code: 9}, {char: 'q', code: 81}, {char: 'w', code: 87}, {char: 'e', code: 69},
+    {char: 'r', code: 82}, {char: 't', code: 84}, {char: 'y', code: 89}, {char: 'u', code: 85},
+    {char: 'i', code: 73}, {char: 'O', code: 79}, {char: 'p', code: 80}, {char: '[', special: '{', code: 219},
+    {char: ']', special: '}', code: 221}, {char: '\\', special: '|', code: 220}
+  ],
+  [
+    {char: 'CapsLock', widthMultiple: 2, code: 20}, {char: 'a', code: 65}, {char: 's', code: 83}, {char: 'd', code: 68},
+    {char: 'f', code: 70}, {char: 'g', code: 71}, {char: 'h', code: 72}, {char: 'j', code: 74}, {char: 'k', code: 75},
+    {char: 'l', code: 76}, {char: ';', special: ':', code: 59}, {char: '\'', special: '"', code: 222}, {char: 'Enter', widthMultiple: 2, code: 13}
+  ],
+  [
+    {char: 'Shift', widthMultiple: 2.5, code: 16}, {char: 'z', code: 90}, {char: 'x', code: 88}, {char: 'c', code: 67}, {char: 'v', code: 86},
+    {char: 'b', code: 66}, {char: 'n', code: 78}, {char: 'm', code: 77}, {char: ',', special: '<', code: 188}, {char: '.', special: '>', code: 190},
+    {char: '/', special: '?', code: 191}, {char: 'Shift', widthMultiple: 2.5, code: 16}
+  ],
+  [
+    {char: 'Ctrl', code: 17}, {char: 'Fn', code: 0}, {char: 'Win', code: 0}, {char: 'Alt', code: 18}, {char: 'Space', widthMultiple: 5, align: 'center', code: 32},
+    {char: 'Alt', code: 18}, {char: 'Ctrl', code: 17}, {char: '←', widthMultiple: 1.5, code: 37}, {char: '→', widthMultiple: 1.5, code: 39}
+  ]
+]
+    
